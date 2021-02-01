c7dff4c5c5ce8d2fde70644db09070bd06b49866
Matt Castillo <mattcastillo2010@gmail.com>
2021-01-31 22:55:25 -0600

    Trying something

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_ENDPOINT: process.env.REACT_APP_API_BASE_URL ||
+  API_ENDPOINT: process.env.API_ENDPOINT ||
     "http://localhost:8000/api",
   API_KEY: process.env.REACT_APP_API_KEY,
   GOOGLE_API_KEY: process.env.REACT_APP_GOOGLE_API_KEY,
