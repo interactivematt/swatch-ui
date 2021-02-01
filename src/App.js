f799983fbc56db1799859ccfcc9abb11dad639d2
Matt Castillo <mattcastillo2010@gmail.com>
2021-01-31 23:18:00 -0600

    Trying something
    changed config to heroku

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
         method: 'GET',
         headers: {
           'content-type': 'application/json',
+          'Accept': 'application/json',
           'Authorization': `Bearer ${config.API_KEY}`
         }
       }),
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   API_ENDPOINT: process.env.API_ENDPOINT ||
-    "http://localhost:8000/api",
+    "https://swatch-ui.herokuapp.com/api",
   API_KEY: process.env.REACT_APP_API_KEY,
   GOOGLE_API_KEY: process.env.REACT_APP_GOOGLE_API_KEY,
 }
