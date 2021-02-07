48efdaa4c3f08bf1a89b21fc8f7866b8cf62bdcc
Matt Castillo <mattcastillo2010@gmail.com>
2021-02-07 00:34:00 -0600

    Fixed config?

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  REACT_APP_API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:8000/api',
+  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
   API_KEY: process.env.REACT_APP_API_KEY,
   GOOGLE_API_KEY: process.env.REACT_APP_GOOGLE_API_KEY,
 }
